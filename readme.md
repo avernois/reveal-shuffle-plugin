17a4882110b6a54922417a185990239e425257f8
Antoine <antoine@crafting-labs.fr>
2015-10-07 13:23:38 +0200

    using array play forward/backward the shuffled deck
    but it won't reshuffle it.

--- readme.md
+++ readme.md
@@ -13,15 +13,22 @@ Add the shuffle folder into your reveal.js plugin folder and include the followi
 ```
 
 ## Usage
-Use space or enter keys to play the next shuffle slides. Using left/righ arrows plays slides the usual way (this may change in a future).
+Use space, enter or right arrow to go forward the next shuffle slide. Use left arrow to fo backward.
 
+
+## Notes
+
+* The shuffling is only done once (at loading time), so left/right arrows will always play the deck in the same order.
+If you want to reshuffle, just reload the page.
+* The first page (landing page) will always the first page of your deck. It is not included in the shuffling.
+* This plugin is known to work with master branch as of 2015/10/06. It may, or may not, work with previous or future versions :)
+* I'm not sure about the `async: true` in the setup. Don't really know what it does.
 
 ## Known issues
 
 * this is a very early work in progress (and I'm not yet convinced it will never become something else :)
 * it only works with 'flat' presentation (behaviour when there are nested slides is undefined)
 * it's an all or nothing.
-* it's not possible not go back to the previous slides
 
 ## Todo
 
--- shuffle/shuffle.js
+++ shuffle/shuffle.js
@@ -1,28 +1,50 @@
+function Shuffled () {
+	function shuffle(array) {
+		var currentIndex = array.length, temporaryValue, randomIndex ;
+	 	while (0 !== currentIndex) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+	}
+	
+	var shuffled = [];
+	for (var i = 1; i != Reveal.getTotalSlides(); ++i) shuffled.push(i)
+	
+	shuffle(shuffled);
+	var currentIndex = -1;
 
-function shuffle(array) {
-		 var currentIndex = array.length, temporaryValue, randomIndex ;
-	 while (0 !== currentIndex) {
-	    randomIndex = Math.floor(Math.random() * currentIndex);
-	    currentIndex -= 1;
-	    temporaryValue = array[currentIndex];
-	    array[currentIndex] = array[randomIndex];
-	    array[randomIndex] = temporaryValue;
+	this.next = function() {
+		currentIndex += 1;
+		Reveal.slide(shuffled[currentIndex]);
+	}
+
+	this.previous = function () {
+		currentIndex -= 1;
+		Reveal.slide(shuffled[currentIndex]);
 	}
 }
 
-var shuffled = [];
-for (var i = 1; i != Reveal.getTotalSlides(); ++i) shuffled.push(i)
 
-shuffle(shuffled);
+var shuffled = new Shuffled();
+
 
 Reveal.configure({
 		keyboard: {
-		13: function() {
-			Reveal.slide(shuffled.pop()),
-			console.log(shuffled);}, // go to the next slide when the ENTER key is pressed
-		27: function() {}, // do something custom when ESC is pressed
-		32: function() {
-			Reveal.slide(shuffled.pop()),
-			console.log(shuffled);}
+		13: function() { // enter
+				shuffled.next();
+			},
+		32: function() { // space
+				shuffled.next();
+			},
+		37: function() { // left
+				shuffled.previous();
+		},
+		39: function() { // right
+				shuffled.next();
+		}
+
   }
 });
